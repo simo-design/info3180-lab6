5ac5aaa51afce67d448ad7f5e2dcb76930ea10c7
simo.design <nadrine.simms@mymona.uwi.mona>
2021-04-04 14:47:52 -0400

    Edited app.js and index file

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -35,6 +35,21 @@ app.component('app-header', {
   }
 });
 
+app.component('news-list', {
+  name: 'NewsList',
+  template: `
+    <div class="news">
+      <h2>News</h2>
+        <ul class="news__list">
+          <li class="news__item">News item 1</li>
+          <li class="news__item">News item 2</li>
+          <li class="news__item">News item 3</li>
+        </ul>
+    </div>
+  `
+});
+
+
 app.component('app-footer', {
   name: 'AppFooter',
   template: `
